79916a371890ca6b2b3368787234faca163379f2
Suvesh Pagam <156836161+Suvesh0707@users.noreply.github.com>
2025-03-02 23:25:51 +0530

    01partyapp

---
+++ 01PartyApp/main.py
@@ -0,0 +1,8 @@
+choice = input("select \n 1.Party \n 2.weeding")
+
+if(choice == 1):
+    print("Welcome to party\n Enjoy your day")
+elif(choice == 2):
+    print("welcome to weeding \n Enjoy the Weeding")
+else:
+    print("Invalid Choice ")
