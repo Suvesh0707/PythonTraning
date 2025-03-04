48b84ba088fdd7fc3b3a9001bc0a66220bea5b23
Suvesh Pagam <156836161+Suvesh0707@users.noreply.github.com>
2025-03-04 12:03:38 +0530

    jeep added

--- 06StarPatternCar/main.py
+++ 06StarPatternCar/main.py
@@ -1,25 +1,39 @@
-
-rows = 12
+rows = 13
 columns = 40
+print("1. car \n2. jeep")
+car = int(input("Enter your choice: "))
 
-for i in range(rows):
-    for j in range(columns):
-        if (
-            (i == 0 and 10 <= j <= 29) or
-            (i == 1 and (j == 9 or j == 30)) or
-            (i == 2 and (8 <= j <= 31)) or
-            (i == 3 and (7 <= j <= 32)) or
+if car == 1:
+    for i in range(rows):
+        for j in range(columns):
+            if (
+                (i == 0 and 10 <= j <= 29) or
+                (i == 1 and (j == 9 or j == 30)) or
+                (i == 2 and (8 <= j <= 31)) or
+                (i == 3 and (7 <= j <= 32)) or
+                (4 <= i <= 7 and (2 <= j <= 37)) or
+                ((i == 8 or i == 10) and (j == 5 or j == 7 or j == 29 or j == 31)) or 
+                (i == 9 and (4 <= j <= 8 or 28 <= j <= 32))
+            ):
+                print("*", end="")
+            else:
+                print(" ", end="")
+        print()
 
-            (4 <= i <= 7 and (j >= 2 and j <= 37)) or
-            # (i == 4 and (2 <= j <= 37)) or
-            # (i == 5 and (2 <= j <= 37)) or
-            # (i ==  6 and (2 <= j <= 37)) or
-            # (i == 7 and (2 <= j <= 37)) or
-            
-            ((i == 8 or i == 10) and (j == 5 or j == 7 or j == 29 or j == 31)) or 
-            (i == 9 and (4 <= j <= 8 or 28 <= j <= 32)) 
-        ):
-            print("*", end="")
-        else:
-            print(" ", end="")
-    print()  
+elif car == 2:
+    for i in range(rows):
+        for j in range(columns):
+            if (
+                (4 <= i <= 6 and 4 <= j <= 20) or
+                (7 <= i <= 9 and 4 <= j <= 30) or
+                (i == 10 and (j == 6 or j == 9 or j == 22 or j == 25)) or
+                (i == 11 and (5 <= j <= 10 or 21 <= j <= 26)) or
+                (i == 12 and (j == 6 or j == 9 or j == 22 or j == 25))
+            ):
+                print("*", end="")
+            else:
+                print(" ", end="")
+        print()
+
+else:
+    print("Invalid input")
--- 06StarPatternCar/tempCodeRunnerFile.py
+++ 06StarPatternCar/tempCodeRunnerFile.py
@@ -1,2 +1,1 @@
-            # (i == 2 and (j == 13 or j == 20 or j == 27)) or
-            # (i == 3 and (j == 13 or j == 20 or j == 27)) or
+ 
